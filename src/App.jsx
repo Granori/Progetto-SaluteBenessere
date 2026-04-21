cbe16ee9ba517da5b51881543ab2e64237a21f1b
Riccardo Granelli <sriccardo.granelli@itis.pr.it>
2026-04-21 11:07:14 +0200

    Rimossi template e aggiornato switch darkmode usando peer

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Navbar from './pages/navbar';
 import Home from './pages/home';
 import PagBMI from './pages/bmi';
 import PagCalorie from './pages/conta_calorie';
-import './App.css';
 
 function App() {
   const [isOpenSettings, setIsOpenSettings] = useState(false);
@@ -34,28 +33,33 @@ function App() {
                 </label>
                 
                 {/* Switch */}
-                <button
-                  onClick={() => setIsDark(!isDark)}
-                  className={`${isDark ? 'bg-gray-500' : 'bg-gray-100'} w-20 h-9 flex items-center rounded-full p-1 transition-colors duration-300`}
-                >
+                <label className="relative w-20 h-9 flex items-center rounded-full p-1 cursor-pointer transition-colors duration-300 has-[:checked]:bg-gray-500 bg-gray-100">
+                  
+                  {/* Input invisibile (Peer) */}
+                  <input 
+                    type="checkbox" 
+                    className="sr-only peer" 
+                    checked={isDark}
+                    onChange={() => setIsDark(!isDark)} 
+                  />
 
-                  {/* Peer */}
+                  {/* Pallino (Reagisce allo stato del peer) */}
                   <div
-                    className={`w-8 h-8 flex items-center justify-center rounded-full transition-transform duration-500 shadow-sm ${
-                      isDark ? 'translate-x-10 bg-gray-700 text-white rotate-360' : 'translate-x-0 bg-white text-yellow-400'
-                    }`}
+                    className="w-8 h-8 flex items-center justify-center rounded-full bg-white text-yellow-400 shadow-sm transition-all duration-500 
+                    peer-checked:translate-x-10 peer-checked:bg-gray-700 peer-checked:text-white peer-checked:rotate-360"
                   >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                    { !isDark ?
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
-                      :
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
-                    }
+                      { !isDark ? (
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+                      ) : (
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+                      )}
                     </svg>
 
                   </div>
 
-                </button>
+                </label>
+
               </div>
 
               <div className='mt-auto'>Accedi</div>
